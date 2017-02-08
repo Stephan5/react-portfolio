fb8786d51260ece93a54c9ffc97537974d514c56
Stephan Blakeslee <github@blakeslee.uk>
2017-02-08 23:44:23 +0000

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 
-# Performs a release of a new version of the library. This script will fail
+# Performs a release of a new version of the site. This script will fail
 # if the working directory is not clean (in git terms).
-if [ -z "$1" ]; then
-   echo "You must specify a new version level: [patch, minor, major]";
-   exit 1;
-fi
+
+#clean and build semantic ui
+rm -rf semantic
+cd semantic
+gulp build
+cd ..
 
 # install dependencies and run build
-yarn clean
+rm -rf dist
+rm -rf static
 yarn install
 yarn build
 
 #copy dist folder and name 'static'
 cp -r dist static
-mkdir prod-release-v$1
-# bump the version
 
-# npm version creates a version commit and tag
+mkdir prod
+cd prod
+cp -r ../static .
+cd ..
 
-git push
-git push --tags
+# push to s3 some how
 
 # cleanup
 rm -rf dist/
-mkdir out
-
-# make the docs
-yarn make-docs
-VERSION=`ls docs/starling-developer-sdk`
-mv docs/starling-developer-sdk/* out/
-rm -rf docs/
-
-git checkout gh-pages
-mv out/* docs/
-echo $VERSION >> _data/versions.csv
-git add .
-git commit -m "Adding docs for version v$VERSION"
-git push
+rm -rf static/
