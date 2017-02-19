2e20761ca142dfe4635d2701e38eac96954e6bbd
Stephan <github@blakeslee.uk>
2017-02-19 22:27:33 +0000

    remove redundant code

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -11,20 +11,12 @@ export default class Footer extends Component {
             <Icon name='setting' loading color="black"/>
             Site under construction
           </Header>
-
           <div>
             <Progress percent="40" indicating color='teal'/>
           </div>
-
-          {/*<div className="text-center">*/}
-          {/*<Button icon="github alternate" content="See my Github" labelPosition="left"*/}
-          {/*href="https://github.com/stephan5" positive/>*/}
-          {/*</div>*/}
         </Segment>
       </Grid>
         <Grid>
-
-
         <Button.Group size="large" basic>
           <Button href={personalInfo.email} target="_blank">
             <Icon name='mail' /> Contact
@@ -42,21 +34,7 @@ export default class Footer extends Component {
         <Button color='linkedin' href={personalInfo.cv} target="_blank">
           <Icon name='file text' /> CV
         </Button>
-
-
         </Button.Group>
-        {/*<Menu compact icon='labeled'>*/}
-          {/*<Menu.Item*/}
-            {/*href={personalInfo.email}><Icon name='mail'/>Contact</Menu.Item>*/}
-          {/*<Menu.Item*/}
-            {/*href={personalInfo.github}><Icon name='github alternate'/>Github</Menu.Item>*/}
-          {/**/}
-          {/*<Menu.Item*/}
-            {/*href={personalInfo.linkedIn}><Icon name='linkedin'/>LinkedIn</Menu.Item>*/}
-          {/**/}
-          {/*<Menu.Item*/}
-            {/*href={personalInfo.cv}><Icon name='file text'/> CV </Menu.Item>*/}
-        {/*</Menu>*/}
       </Grid>
       </div>
     );
