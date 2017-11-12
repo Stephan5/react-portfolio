df380e92f2aafbba7eb9cb4dd702f31983893900
Stephan <stephan.github@blakeslee.uk>
2017-11-12 16:33:12 +0000

    STAR-

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,17 @@
 
 # Performs a release of a new version of the site. This script will fail
 # if the working directory is not clean (in git terms).
+rm -rf deploy static dist
 
 # install dependencies and run build
 npm run build
 
-#copy dist folder and name 'static'
-cp -r dist static
-
 mkdir deploy
 cd deploy
-cp -r ../static .
+cp -r ../dist ./static
+cp -r ../public/* ./static
 cp -r ../index.html .
 cp -r ../error.html .
 cd ..
+
+rm -rf dist
